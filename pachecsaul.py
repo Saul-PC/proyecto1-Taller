34d7be86677e72c43934d2fbf56ddbfa33086c40
Saul-PC <166790427+Saul-PC@users.noreply.github.com>
2024-04-12 23:11:45 -0600

    Update pachecsaul.py

--- pachecsaul.py
+++ pachecsaul.py
@@ -1,16 +1,4 @@
-import time
-t0 = time.time()
-def sepDig(n):
-    if n < 10000:
-        d1 = n % 10
-        n //= 10
-        d2 = n % 10
-        n //= 10
-        d3 = n % 10
-        n //= 10
-        d4 = n 
-        return [d4,d3,d2,d1]
-    if n >= 10000: 
+def sepDig(n): 
         d1 = n % 10
         n //= 10
         d2 = n % 10
@@ -21,25 +9,28 @@ def sepDig(n):
         n //= 10
         d5 = n
         return [d5,d4,d3,d2,d1]
-    
-    
+"""
+Dominio: un número entero de 4 o 5 dígitos
+Codominio: Una lista de 5 números enteros
 
-
-def digRep(l,cantreps):
+"""
+def digRep(l,r):
     for i in l:
-        if l.count(i) > cantreps:
+        if l.count(i) > r:
             return False
     return True
+"""
+Dominio: una lista de 10 números enteros y un entero r
+Codominio: Verdadero si en la lista de 10 enteros se repiten
+cada uno de sus dígitos r veces o menos, falso si esto no se cumple
 
-
+"""
 
 t = int(input())
-
 while t:
 
     t -= 1
     c, r = map(int, input().split())
-
     
     for den in range(10000//c,100000//c):
 
@@ -48,11 +39,6 @@ while t:
         listaNum = sepDig(num)
 
         if len(listaDen) >= 4:
-
-            cerosLista = 5 - len(listaDen)
-
-            for i in range(cerosLista):
-                listaDen.insert(0,0)
         
             if digRep(listaDen + listaNum, r):
                 print(str(num) + "/" + str(den) + "=" + str(c))
